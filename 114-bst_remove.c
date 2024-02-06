1327beae00b9256c4c92b1c0add97a9d540f8a01
AlexDelei <alexdelei125@gmail.com>
2024-02-06 01:57:24 -0800

    Binary trees and Binary Search trees

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -14,6 +14,19 @@ bst_t *find_successor(bst_t *node)
 	return (succ);
 }
 /**
+ * find_predecessor - Finds the in-order predecessor of a node
+ * @node: Pointer to the node
+ * Return: Pointer to the in-order predecessor
+ */
+bst_t *find_predecessor(bst_t *node)
+{
+	bst_t *pred = node->left;
+
+	while (pred->right != NULL)
+		pred = pred->right;
+	return (pred);
+}
+/**
  * bst_remove - Removes a node from a Binary Search Tree
  * @root: Pointer to the root node of the tree
  * @value: Value to remove from the tree
@@ -22,7 +35,7 @@ bst_t *find_successor(bst_t *node)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *succ, *temp;
+	bst_t *succ, *pred, *temp;
 
 	if (root == NULL)
 		return (NULL);
@@ -46,6 +59,9 @@ bst_t *bst_remove(bst_t *root, int value)
 			free(root);
 			return (temp);
 		}
+		pred = find_predecessor(root);
+		root->n = pred->n;
+		root->left = bst_remove(root->left, pred->n);
 		succ = find_successor(root);
 		root->n = succ->n;
 		root->right = bst_remove(root->right, succ->n);
